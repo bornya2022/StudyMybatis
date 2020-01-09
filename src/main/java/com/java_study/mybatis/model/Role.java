beaf4d629689d0841a4fe4e07e18a7ebca3fa1c0
xjj <752534553@qq.com>
2020-01-09 10:01:15 +0800

    缓存相关学习 一级缓存，二级缓存的实现与测试

--- src/main/java/com/java_study/mybatis/model/Role.java
+++ src/main/java/com/java_study/mybatis/model/Role.java
@@ -1,8 +1,14 @@
 package com.java_study.mybatis.model;
 
 
+import java.io.Serializable;
 
-public class Role {
+/**
+ * 实现Serializable接口，使Role可序列化
+ * Mybatis二级缓存开启默认要求对应的POJO对象是可序列化的
+ */
+public class Role implements Serializable {
+    public static final long serialVersionUID=598736524547906734L;
     private Long id;
     private String roleName;
     private String note;
